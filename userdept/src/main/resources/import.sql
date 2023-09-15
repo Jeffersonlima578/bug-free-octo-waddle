0769f13c5d1a05cc2a32c23f130c6a94b5b83fce
Jefferson de Lima Gomes <jefferson.limagomes@hotmail.com>
2023-09-15 11:53:40 -0300

    Banco de Dados com JAVA, SpringToo4 e o Postman

---
+++ userdept/src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO tb_department(name) VALUES ('Gestão');
+INSERT INTO tb_department(name) VALUES ('Informática');
+
+
+INSERT INTO tb_user(department_id, name, email) VALUES (1,'Maria', 'maria@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (1,'bob', 'bob@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (1,'Chaves', 'chaves@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (1,'Ana', 'ana@gmail.com');
